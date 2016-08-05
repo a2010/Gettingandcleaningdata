ab3a1c4beaebaa57b1dcda1f3caa624744e94231
a2010 <2007.apoorva@gmail.com>
2016-08-05 00:43:46 -0400

    Update run_analysis.R

--- run_analysis.R
+++ run_analysis.R
@@ -1,81 +1,49 @@
 
-+## "Human Activity Recognition on Smartphones using a Multiclass Hardware-Friendly Support Vector Machine".
-+## International Workshop of Ambient Assisted Living (IWAAL 2012). Vitoria-Gasteiz, Spain. Dec 2012
-+
-+## This script merges data from a number of .txt files and produces 
-+## a tidy data set which may be used for further analysis.
-+
-+##check for required packages
-+if (!("reshape2" %in% rownames(installed.packages())) ) {
-+  print("Please install required package \"reshape2\" before proceeding")
-+} else {
-+        ## Open required libraries
-+        library(reshape2)
-+
-+        ## First, read all required .txt files and label the datasets
-+        
-+        ## Read all activities and their names and label the aproppriate columns 
-+        activity_labels <- read.table("./activity_labels.txt",col.names=c("activity_id","activity_name"))
-+
-+        ## Read the dataframe's column names
-+        features <- read.table("features.txt")
-+        feature_names <-  features[,2]
-+        
-+        ## Read the test data and label the dataframe's columns
-+        testdata <- read.table("./test/X_test.txt")
-+        colnames(testdata) <- feature_names
-+        
-+        ## Read the training data and label the dataframe's columns
-+        traindata <- read.table("./train/X_train.txt")
-+        colnames(traindata) <- feature_names
-+        
-+        ## Read the ids of the test subjects and label the the dataframe's columns
-+        test_subject_id <- read.table("./test/subject_test.txt")
-+        colnames(test_subject_id) <- "subject_id"
-+        
-+        ## Read the activity id's of the test data and label the the dataframe's columns
-+        test_activity_id <- read.table("./test/y_test.txt")
-+        colnames(test_activity_id) <- "activity_id"
-+        
-+        ## Read the ids of the test subjects and label the the dataframe's columns
-+        train_subject_id <- read.table("./train/subject_train.txt")
-+        colnames(train_subject_id) <- "subject_id"
-+        
-+        ## Read the activity id's of the training data and label 
-+        ##the dataframe's columns
-+        train_activity_id <- read.table("./train/y_train.txt")
-+        colnames(train_activity_id) <- "activity_id"
-+        
-+        ##Combine the test subject id's, the test activity id's 
-+        ##and the test data into one dataframe
-+        test_data <- cbind(test_subject_id , test_activity_id , testdata)
-+        
-+        ##Combine the test subject id's, the test activity id's 
-+        ##and the test data into one dataframe
-+        train_data <- cbind(train_subject_id , train_activity_id , traindata)
-+        
-+        ##Combine the test data and the train data into one dataframe
-+        all_data <- rbind(train_data,test_data)
-+        
-+        ##Keep only columns refering to mean() or std() values
-+        mean_col_idx <- grep("mean",names(all_data),ignore.case=TRUE)
-+        mean_col_names <- names(all_data)[mean_col_idx]
-+        std_col_idx <- grep("std",names(all_data),ignore.case=TRUE)
-+        std_col_names <- names(all_data)[std_col_idx]
-+        meanstddata <-all_data[,c("subject_id","activity_id",mean_col_names,std_col_names)]
-+        
-+        ##Merge the activities datase with the mean/std values datase 
-+        ##to get one dataset with descriptive activity names
-+        descrnames <- merge(activity_labels,meanstddata,by.x="activity_id",by.y="activity_id",all=TRUE)
-+        
-+        ##Melt the dataset with the descriptive activity names for better handling
-+        data_melt <- melt(descrnames,id=c("activity_id","activity_name","subject_id"))
-+        
-+        ##Cast the melted dataset according to  the average of each variable 
-+        ##for each activity and each subjec
-+        mean_data <- dcast(data_melt,activity_id + activity_name + subject_id ~ variable,mean)
-+       
-+        ## Create a file with the new tidy dataset
-+        write.table(mean_data,"./tidy_movement_data.txt")
-+
-+}
+ filename <- "getdata_dataset.zip"
+
+## Download and unzip the dataset:
+if (!file.exists(filename)){
+  fileURL <- "https://d396qusza40orc.cloudfront.net/getdata%2Fprojectfiles%2FUCI%20HAR%20Dataset.zip "
+  download.file(fileURL, filename, method="curl")
+}  
+if (!file.exists("UCI HAR Dataset")) { 
+  unzip(filename) 
+}
+
+# Load activity labels + features
+activityLabels <- read.table("UCI HAR Dataset/activity_labels.txt")
+activityLabels[,2] <- as.character(activityLabels[,2])
+features <- read.table("UCI HAR Dataset/features.txt")
+features[,2] <- as.character(features[,2])
+
+# Extract only the data on mean and standard deviation
+featuresWanted <- grep(".*mean.*|.*std.*", features[,2])
+featuresWanted.names <- features[featuresWanted,2]
+featuresWanted.names = gsub('-mean', 'Mean', featuresWanted.names)
+featuresWanted.names = gsub('-std', 'Std', featuresWanted.names)
+featuresWanted.names <- gsub('[-()]', '', featuresWanted.names)
+
+
+# Load the datasets
+train <- read.table("UCI HAR Dataset/train/X_train.txt")[featuresWanted]
+trainActivities <- read.table("UCI HAR Dataset/train/Y_train.txt")
+trainSubjects <- read.table("UCI HAR Dataset/train/subject_train.txt")
+train <- cbind(trainSubjects, trainActivities, train)
+
+test <- read.table("UCI HAR Dataset/test/X_test.txt")[featuresWanted]
+testActivities <- read.table("UCI HAR Dataset/test/Y_test.txt")
+testSubjects <- read.table("UCI HAR Dataset/test/subject_test.txt")
+test <- cbind(testSubjects, testActivities, test)
+
+# merge datasets and add labels
+allData <- rbind(train, test)
+colnames(allData) <- c("subject", "activity", featuresWanted.names)
+
+# turn activities & subjects into factors
+allData$activity <- factor(allData$activity, levels = activityLabels[,1], labels = activityLabels[,2])
+allData$subject <- as.factor(allData$subject)
+
+allData.melted <- melt(allData, id = c("subject", "activity"))
+allData.mean <- dcast(allData.melted, subject + activity ~ variable, mean)
+
+write.table(allData.mean, "tidy.txt", row.names = FALSE, quote = FALSE)
